8c2fefcd9014202ec606f072d1ea01d993bea437
No Elka <no.elka.the.god@gmail.com>
2025-01-24 11:05:07 +0300

    Procrastination in progress

--- scripts/player_selector.gd
+++ scripts/player_selector.gd
@@ -7,16 +7,18 @@ var timer := 0
 var player_ready := false
 var text_to_print: String
 var printing_progress: int = -1
+var is_in_lobby := true
 
 @export var character_type_descriptions: Array[String]
 
 @onready var label_of_readiness := $Ready
-@onready var player_sprite := $Buttons/Player
-@onready var buttons := $Buttons
-@onready var button_up := $Buttons/Up
-@onready var button_down := $Buttons/Down
-@onready var button_left := $Buttons/Left
-@onready var button_right := $Buttons/Right
+@onready var player_sprite := $LobbyButtons/Player
+@onready var buttons := $LobbyButtons
+@onready var other_buttons := $NotLobbyButtons
+@onready var button_up := $LobbyButtons/Up
+@onready var button_down := $LobbyButtons/Down
+@onready var button_left := $LobbyButtons/Left
+@onready var button_right := $LobbyButtons/Right
 @onready var stat_panel := $Stats
 @onready var sound := $PressDoundEmitter
 
@@ -34,7 +36,7 @@ func _ready() -> void:
 	button_left.texture.region = Rect2(0, 0, 32, 32)
 	button_right.texture.region = Rect2(32, 0, 32, 32)
 	
-func yo_wassup(player: Player) -> void:
+func yo_wassup(player: Player, is_lobby: bool = true) -> void:
 	bound_player = player
 	visible = true
 	player_ready = false
@@ -44,12 +46,21 @@ func yo_wassup(player: Player) -> void:
 	player_sprite.texture.atlas = load("res://sprites/player.png")
 	player_sprite.texture.region = Rect2(bound_player.character_color * 48, bound_player.character_type * 48, 48, 48)
 	update_stat_text(bound_player.character_type)
+	is_in_lobby = is_lobby
 	if position.x > 1000:
 		label_of_readiness.size_flags_horizontal = SIZE_SHRINK_END
 		buttons.size_flags_horizontal = SIZE_SHRINK_END
+		other_buttons.size_flags_horizontal = SIZE_SHRINK_END
 	else:
 		label_of_readiness.size_flags_horizontal = SIZE_SHRINK_BEGIN
 		buttons.size_flags_horizontal = SIZE_SHRINK_BEGIN
+		other_buttons.size_flags_horizontal = SIZE_SHRINK_BEGIN
+	if is_lobby:
+		buttons.visible = true
+		other_buttons.visible = false
+	else:
+		buttons.visible = false
+		other_buttons.visible = true
 
 func round_started() -> void:
 	bound_player.bound_player_selector = null
@@ -58,64 +69,69 @@ func round_started() -> void:
 
 func left_pressed():
 	sound.play()
-	timer = 6
-	button_left.texture.region.position.y = 32
-	var prev_color = bound_player.character_color
-	for i in range(4):
-		bound_player.character_color += 1
-		if bound_player.character_color > 3: bound_player.character_color = 0
-		if bound_player.game_manager.player_color_numbers[bound_player.character_color]:
-			bound_player.game_manager.player_color_numbers[prev_color] = true
-			break
-	bound_player.game_manager.player_color_numbers[bound_player.character_color] = false
-	bound_player.change_appearence()
-	player_sprite.texture.region = Rect2(bound_player.character_color * 48, bound_player.character_type * 48, 48, 48)
+	if is_in_lobby:
+		timer = 6
+		button_left.texture.region.position.y = 32
+		var prev_color = bound_player.character_color
+		for i in range(4):
+			bound_player.character_color += 1
+			if bound_player.character_color > 3: bound_player.character_color = 0
+			if bound_player.game_manager.player_color_numbers[bound_player.character_color]:
+				bound_player.game_manager.player_color_numbers[prev_color] = true
+				break
+		bound_player.game_manager.player_color_numbers[bound_player.character_color] = false
+		bound_player.change_appearence()
+		player_sprite.texture.region = Rect2(bound_player.character_color * 48, bound_player.character_type * 48, 48, 48)
 
 func right_pressed():
 	sound.play()
-	timer = 6
-	button_right.texture.region.position.y = 32
-	var prev_color = bound_player.character_color
-	for i in range(4):
-		bound_player.character_color -= 1
-		if bound_player.character_color < 0: bound_player.character_color = 3
-		if bound_player.game_manager.player_color_numbers[bound_player.character_color]:
-			bound_player.game_manager.player_color_numbers[prev_color] = true
-			break
-	bound_player.game_manager.player_color_numbers[bound_player.character_color] = false
-	bound_player.change_appearence()
-	player_sprite.texture.region = Rect2(bound_player.character_color * 48, bound_player.character_type * 48, 48, 48)
+	if is_in_lobby:
+		timer = 6
+		button_right.texture.region.position.y = 32
+		var prev_color = bound_player.character_color
+		for i in range(4):
+			bound_player.character_color -= 1
+			if bound_player.character_color < 0: bound_player.character_color = 3
+			if bound_player.game_manager.player_color_numbers[bound_player.character_color]:
+				bound_player.game_manager.player_color_numbers[prev_color] = true
+				break
+		bound_player.game_manager.player_color_numbers[bound_player.character_color] = false
+		bound_player.change_appearence()
+		player_sprite.texture.region = Rect2(bound_player.character_color * 48, bound_player.character_type * 48, 48, 48)
 
 func up_pressed():
 	sound.play()
-	timer = 6
-	button_up.texture.region.position.y = 32
-	bound_player.character_type += 1
-	if bound_player.character_type > 3: bound_player.character_type = 0
-	update_stat_text(bound_player.character_type)
-	bound_player.change_appearence()
-	player_sprite.texture.region = Rect2(bound_player.character_color * 48, bound_player.character_type * 48, 48, 48)
+	if is_in_lobby:
+		timer = 6
+		button_up.texture.region.position.y = 32
+		bound_player.character_type += 1
+		if bound_player.character_type > 3: bound_player.character_type = 0
+		update_stat_text(bound_player.character_type)
+		bound_player.change_appearence()
+		player_sprite.texture.region = Rect2(bound_player.character_color * 48, bound_player.character_type * 48, 48, 48)
 
 func down_pressed():
 	sound.play()
-	timer = 6
-	button_down.texture.region.position.y = 32
-	bound_player.character_type -= 1
-	if bound_player.character_type < 0: bound_player.character_type = 3
-	update_stat_text(bound_player.character_type)
-	bound_player.change_appearence()
-	player_sprite.texture.region = Rect2(bound_player.character_color * 48, bound_player.character_type * 48, 48, 48)
+	if is_in_lobby:
+		timer = 6
+		button_down.texture.region.position.y = 32
+		bound_player.character_type -= 1
+		if bound_player.character_type < 0: bound_player.character_type = 3
+		update_stat_text(bound_player.character_type)
+		bound_player.change_appearence()
+		player_sprite.texture.region = Rect2(bound_player.character_color * 48, bound_player.character_type * 48, 48, 48)
 
 func fire_pressed():
 	sound.play()
-	player_ready = not player_ready
-	bound_player.game_manager.set_ready(player_ready)
-	if player_ready:
-		label_of_readiness.self_modulate = Color(0.2, 1, 0.2)
-		label_of_readiness.text = "Ready"
-	else:
-		label_of_readiness.self_modulate = Color(0.5, 0, 0)
-		label_of_readiness.text = "Not ready"
+	if is_in_lobby:
+		player_ready = not player_ready
+		bound_player.game_manager.set_ready(player_ready)
+		if player_ready:
+			label_of_readiness.self_modulate = Color(0.2, 1, 0.2)
+			label_of_readiness.text = "Ready"
+		else:
+			label_of_readiness.self_modulate = Color(0.5, 0, 0)
+			label_of_readiness.text = "Not ready"
 
 func update_stat_text(c_type: int) -> void:
 	stat_panel.text = ""
